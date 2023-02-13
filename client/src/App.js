f3e45c859a440738a880324de44581405ec7cc58
jll38 <jll38@njit.edu>
2023-02-13 17:07:46 -0500

    testing antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
+import 'antd/dist/reset.css';
+import './App.css';
 import React, { useState, useEffect} from 'react'
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer } = Layout;
+
 
 function App() {
 
@@ -15,9 +20,31 @@ useEffect(() => {
 )
 },[])
   return (
-    <div>
-      
-    </div>
+    <>
+      <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(6).fill(null).map((_, index) => {
+            const key = index + 1;
+            return {
+              key,
+              label: `nav ${key}`,
+            };
+          })}
+        />
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content">
+          Content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+    </Layout>
+    </>
   )
 }
 
