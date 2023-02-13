2bc809a5a979edf32b74a95f3c34405892400913
jll38 <jll38@njit.edu>
2023-02-13 16:44:13 -0500

    client server setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
 import React, { useState, useEffect} from 'react'
 
 function App() {
+
+const [data, setData] = useState([{}]);
+
+useEffect(() => {
+  fetch("/users").then(
+    res => res.json()
+  ).then(
+    data => {
+      setData(data)
+      console.log(data)
+    }
+)
+},[])
   return (
     <div>
       
