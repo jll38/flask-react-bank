88efb17b2a923749dba837a150db8786132458cf
jll38 <jll38@njit.edu>
2023-02-13 21:39:48 -0500

    install SQLAlchemy

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+CORS(app)
 
 #Users API router
 @app.route("/users")
 def users():
-    return {"Users": ["John", "Bob", "Juan"]}
+    return {"users": ["John", "Bob", "Juan"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
