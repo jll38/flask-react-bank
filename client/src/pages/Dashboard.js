874c3f6f6875ec0b9715ae2f5b7e98e4025cb0b6
Julian <jll38@njit.edu>
2023-02-15 15:57:22 -0500

    Work on Dashboard and SQL

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -8,7 +8,16 @@ import {
   Stack,
   HStack,
   Divider,
-  VStack
+  VStack,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
 } from '@chakra-ui/react'
 
 
@@ -17,9 +26,52 @@ function Dashboard() {
     <>
       <Heading align='center'>Dashboard</Heading>
       <Stack direction={["column", "row"]} w='100%' spacing='30px'>
-        <Box w='65%' h='500px' borderWidth='2px' borderRadius='lg' p="15px 30px" overflow='hidden'>
-          <Heading size='md'>Account Balance</Heading>
-          <Text>$1,000</Text>
+        <Box w='65%' h='500px' borderWidth='2px' borderRadius='lg' p="25px 30px" overflow='hidden'>
+          <Heading size='lg'>Account Balance</Heading>
+          <Heading size='md'>$1,000</Heading>
+
+          <div id='transactions'>
+          <Heading mt='80px' size='lg'>Transactions</Heading>
+          <TableContainer h='270px' overflowY='scroll'>
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                  <Th>Source</Th>
+                  <Th>Amount</Th>
+                  <Th>Date</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr color='red.500'>
+                  <Td>Withdrawl</Td>
+                  <Td>$400</Td>
+                  <Td> 10/11/22</Td>
+                </Tr>
+                <Tr color='green.500'>
+                  <Td>Deposit</Td>
+                  <Td>$200</Td>
+                  <Td> 10/10/22</Td>
+                </Tr>
+                <Tr color='green.500'>
+                  <Td>Deposit</Td>
+                  <Td>$200</Td>
+                  <Td> 10/19/22</Td>
+                </Tr>
+                <Tr color='green.500'>
+                  <Td>Deposit</Td>
+                  <Td>$200</Td>
+                  <Td> 10/19/22</Td>
+                </Tr>
+                <Tr color='green.500'>
+                  <Td>Deposit</Td>
+                  <Td>$200</Td>
+                  <Td> 10/19/22</Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+          </div>
+
         </Box>
         <VStack w='30%'>
           <Box w='100%' h='250px' borderWidth='2px' borderRadius='lg' p="15px 30px" overflow='hidden'>
@@ -32,7 +84,7 @@ function Dashboard() {
           </Box>
           <Box w='100%' h='250px' borderWidth='2px' borderRadius='lg' p="15px 30px" overflow='hidden'>
             <div>
-              <Heading size='md'>Recent Transactions</Heading>
+              <Heading size='md'>Notifications</Heading>
               <Divider orientation='horizontal' />
             </div>
           </Box>
