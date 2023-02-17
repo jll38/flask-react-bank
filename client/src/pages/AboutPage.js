ec4b05913c4b963c6b162315746a9e7e5a9c4a9b
jll38 <jll38@njit.edu>
2023-02-17 15:02:02 -0500

    Basic about page

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -2,6 +2,7 @@ import 'antd/dist/reset.css';
 import '../App.css';
 import React, { useState, useEffect } from 'react';
 import HeaderDiv from '../components/HeaderDiv';
+import { Text } from '@chakra-ui/react'
 
 
 
@@ -9,6 +10,19 @@ function AboutPage() {
   return (
     <>
       <HeaderDiv text="About Us" />
+      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Molestie a iaculis at erat. Tristique sollicitudin nibh sit amet commodo. Egestas fringilla phasellus faucibus scelerisque. Aliquet lectus proin nibh nisl. Nunc aliquet bibendum enim facilisis. Tristique senectus et netus et malesuada fames ac. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Amet porttitor eget dolor morbi non. Nam libero justo laoreet sit amet cursus sit. Ornare arcu odio ut sem nulla pharetra diam. Massa eget egestas purus viverra.
+        <br />
+        <br />
+        Tempor orci eu lobortis elementum nibh tellus. A pellentesque sit amet porttitor eget dolor morbi non. Blandit aliquam etiam erat velit scelerisque. Urna et pharetra pharetra massa massa. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Ligula ullamcorper malesuada proin libero. Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Iaculis urna id volutpat lacus laoreet non. Eget est lorem ipsum dolor sit amet consectetur adipiscing elit. Dui sapien eget mi proin sed libero enim. Ut sem nulla pharetra diam sit amet.
+        <br />
+        <br />
+        Convallis convallis tellus id interdum. Morbi non arcu risus quis varius quam quisque id. Malesuada fames ac turpis egestas maecenas pharetra convallis posuere morbi. Ullamcorper morbi tincidunt ornare massa. Risus nullam eget felis eget nunc lobortis mattis. Proin sed libero enim sed. Massa tincidunt dui ut ornare lectus sit. Ornare aenean euismod elementum nisi. Nisl nunc mi ipsum faucibus vitae aliquet nec. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet non. Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. Ante in nibh mauris cursus mattis molestie a iaculis. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. In eu mi bibendum neque egestas.
+        <br />
+        <br />
+        Habitasse platea dictumst quisque sagittis purus sit. Imperdiet proin fermentum leo vel orci porta non. Nisl rhoncus mattis rhoncus urna. Neque volutpat ac tincidunt vitae semper quis lectus nulla. At ultrices mi tempus imperdiet nulla. Nibh praesent tristique magna sit amet purus gravida quis blandit. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Et malesuada fames ac turpis egestas. Risus nullam eget felis eget nunc lobortis mattis aliquam. Dui id ornare arcu odio ut sem nulla pharetra.
+        <br />
+        <br />
+        Augue neque gravida in fermentum et sollicitudin. Semper eget duis at tellus at urna. Adipiscing bibendum est ultricies integer quis auctor elit. Venenatis a condimentum vitae sapien. Vehicula ipsum a arcu cursus vitae. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Lobortis scelerisque fermentum dui faucibus in ornare. Libero enim sed faucibus turpis in eu mi bibendum. Bibendum enim facilisis gravida neque convallis a. Viverra nibh cras pulvinar mattis nunc sed blandit. Sem viverra aliquet eget sit amet tellus cras adipiscing. Vehicula ipsum a arcu cursus vitae congue. Arcu cursus euismod quis viverra nibh. Bibendum est ultricies integer quis auctor elit sed vulputate mi. Et pharetra pharetra massa massa ultricies mi quis hendrerit. Nunc mattis enim ut tellus.</Text>
     </>
   );
 }
